80b5ee5fe892fa28320e12bbf93cff7e5491a320
Crystal <crystalarcus@gmail.com>
2026-05-28 17:38:28 +0530

    Updated Home FAB menu UI to Expressive update

--- lib/core/navigation/root_view.dart
+++ lib/core/navigation/root_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
 import 'package:redesigned/core/utils/animations.dart';
 import 'package:redesigned/widgets/utils/open_container.dart'
     as container_transform;
@@ -19,9 +20,11 @@ class RootView extends StatefulWidget {
   State<RootView> createState() => _RootViewState();
 }
 
-class _RootViewState extends State<RootView>
-    with SingleTickerProviderStateMixin {
+class _RootViewState extends State<RootView> with TickerProviderStateMixin {
   int currentIndex = 0;
+  final Curve expressiveFastSpatial = const Cubic(0.42, 1.67, 0.21, 0.90);
+  final Curve expressiveFastEffects = const Cubic(0.31, 0.94, 0.34, 1.00);
+
   late final _barAnimation = BarAnimation(parent: _controller);
   late final _controller = AnimationController(
       duration: const Duration(milliseconds: 1000),
@@ -31,8 +34,11 @@ class _RootViewState extends State<RootView>
   late final _railAnimation = RailAnimation(parent: _controller);
   late final _railFabAnimation = RailFabAnimation(parent: _controller);
 
+  final GlobalKey _fabKey = GlobalKey();
   int selectedIndex = 0;
   bool controllerInitialized = false;
+  bool _isHomeFABMenuOpen = false;
+
   @override
   void initState() {
     super.initState();
@@ -56,9 +62,8 @@ class _RootViewState extends State<RootView>
   }
 
   void _updateNavigation() {
-    final double width = MediaQuery.of(context).size.width;
+    final double width = MediaQuery.sizeOf(context).width;
     final AnimationStatus status = _controller.status;
-
     final bool shouldShow = width > 600;
 
     if (shouldShow) {
@@ -84,78 +89,75 @@ class _RootViewState extends State<RootView>
     super.dispose();
   }
 
-  Widget homeScreenFAB() => FloatingActionButton(
-        heroTag: 'homeFab',
-        onPressed: () {
-          showModalBottomSheet(
-              showDragHandle: true,
-              useRootNavigator: true,
-              context: context,
-              builder: (BuildContext context) {
-                return SafeArea(
-                    child: SizedBox(
-                        height: 125,
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Column(
-                                children: [
-                                  IconButton.filledTonal(
-                                    onPressed: () {},
-                                    padding: const EdgeInsets.all(16),
-                                    icon: const Icon(Icons.add_box_outlined),
-                                    iconSize: 36,
-                                  ),
-                                  const SizedBox(height: 12),
-                                  Text(
-                                    "Post",
-                                    style: GoogleFonts.manrope(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w600),
-                                  )
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  IconButton.filledTonal(
-                                    onPressed: () {},
-                                    padding: const EdgeInsets.all(16),
-                                    icon: const Icon(
-                                        Icons.movie_creation_outlined),
-                                    iconSize: 36,
-                                  ),
-                                  const SizedBox(height: 12),
-                                  Text(
-                                    "Reel",
-                                    style: GoogleFonts.manrope(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w600),
-                                  )
-                                ],
-                              ),
-                              Column(
-                                children: [
-                                  IconButton.filledTonal(
-                                    onPressed: () {},
-                                    padding: const EdgeInsets.all(16),
-                                    icon: const Icon(
-                                        Icons.cast_connected_outlined),
-                                    iconSize: 36,
-                                  ),
-                                  const SizedBox(height: 12),
-                                  Text(
-                                    "Live",
-                                    style: GoogleFonts.manrope(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w600),
-                                  )
-                                ],
-                              ),
-                            ])));
-              });
-        },
-        child: const Icon(Icons.add),
-      );
+  void _toggleMenu() {
+    final RenderBox? renderBox =
+        _fabKey.currentContext?.findRenderObject() as RenderBox?;
+    if (renderBox == null) return;
+
+    final Size fabSize = renderBox.size;
+    final Offset fabOffset = renderBox.localToGlobal(Offset.zero);
+    final Rect sourceRect = fabOffset & fabSize;
+
+    setState(() {
+      _isHomeFABMenuOpen = true;
+    });
+
+    showGeneralDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      barrierLabel: 'Dismiss Contextual FAB Menu',
+      barrierColor: Theme.of(context).colorScheme.scrim.withAlpha(120),
+      transitionDuration: const Duration(milliseconds: 300),
+      pageBuilder: (context, animation, secondaryAnimation) {
+        return const SizedBox.shrink();
+      },
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        // Look here: we can remove the topPadding variable entirely
+        return _ModalMenuOverlay(
+          routeAnimation: animation,
+          sourceRect: sourceRect,
+          expressiveFastSpatial: expressiveFastSpatial,
+          expressiveFastEffects: expressiveFastEffects,
+          onDismissStarted: () {
+            setState(() {
+              _isHomeFABMenuOpen = false;
+            });
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildHomeScreenFAB() {
+    return SizedBox(
+      key: _fabKey,
+      height: 84,
+      width: 84,
+      child: Align(
+        alignment: Alignment.topRight,
+        child: SizedBox(
+          width: 84.0,
+          height: 84.0,
+          child: FloatingActionButton(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: Theme.of(context).colorScheme.onPrimary,
+            heroTag: 'homeFab',
+            onPressed: _toggleMenu,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: const Icon(
+              Icons.add,
+              size: 28,
+              key: ValueKey('m3_fab_icon'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget homeScreenFAB() => _buildHomeScreenFAB();
 
   Widget messageFAB() => container_transform.OpenContainer(
       transitionType: container_transform.ContainerTransitionType.fadeThrough,
@@ -200,14 +202,14 @@ class _RootViewState extends State<RootView>
           children: [currentChild!, ...previousChildren],
         ),
         child: !context.watch<AppService>().isNavBarVisible ||
-                // ? const SizedBox()
-                MediaQuery.sizeOf(context).width > 600
-            ? const SizedBox()
+                MediaQuery.sizeOf(context).width > 600 ||
+                _isHomeFABMenuOpen
+            ? const SizedBox(key: ValueKey('fab_empty'))
             : currentIndex == 0
-                ? homeScreenFAB()
+                ? _buildHomeScreenFAB()
                 : currentIndex == 2
                     ? messageFAB()
-                    : const SizedBox(),
+                    : const SizedBox(key: ValueKey('fab_empty_other')),
       ),
       bottomNavigationBar: AnimatedContainer(
         duration: Durations.medium3,
@@ -235,6 +237,7 @@ class _RootViewState extends State<RootView>
                         break;
                       case 1:
                         context.go('/stories');
+                        break;
                       case 2:
                         context.go('/messages');
                         break;
@@ -281,6 +284,7 @@ class _RootViewState extends State<RootView>
                               break;
                             case 1:
                               context.go('/reels');
+                              break;
                             case 2:
                               context.go('/messages');
                               break;
@@ -301,6 +305,294 @@ class _RootViewState extends State<RootView>
           Expanded(child: widget.child)
         ],
       ),
+    );
+  }
+}
+
+class _ModalMenuOverlay extends StatefulWidget {
+  final Animation<double> routeAnimation;
+  final Rect sourceRect;
+  final Curve expressiveFastSpatial;
+  final Curve expressiveFastEffects;
+  final VoidCallback onDismissStarted;
+
+  const _ModalMenuOverlay({
+    required this.routeAnimation,
+    required this.sourceRect,
+    required this.expressiveFastSpatial,
+    required this.expressiveFastEffects,
+    required this.onDismissStarted,
+  });
+
+  @override
+  State<_ModalMenuOverlay> createState() => _ModalMenuOverlayState();
+}
+
+class _ModalMenuOverlayState extends State<_ModalMenuOverlay>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _localCloseController;
+  late final Animation<double> _exitAnimation;
+  bool _isClosing = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Configure your specialized exit duration (e.g., 140ms for snappy collapse)
+    _localCloseController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 140),
+    );
+    // Configure your exit curve profile (e.g., fastOutSlowIn to prevent visual float)
+    _exitAnimation = CurvedAnimation(
+      parent: _localCloseController,
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  @override
+  void dispose() {
+    _localCloseController.dispose();
+    super.dispose();
+  }
+
+  void _handleClose() {
+    if (_isClosing) return;
+    setState(() {
+      _isClosing = true;
+    });
+    widget.onDismissStarted();
+    _localCloseController.forward().then((_) {
+      if (mounted) {
+        Navigator.of(context).pop();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final Size screenSize = MediaQuery.sizeOf(context);
+
+    final double bottomMargin = screenSize.height - widget.sourceRect.bottom;
+    final double rightMargin = screenSize.width - widget.sourceRect.right;
+    final double fabTargetWidth = widget.sourceRect.width * (56.0 / 84.0);
+    final double fabTargetHeight = widget.sourceRect.height * (56.0 / 84.0);
+
+    final double targetLeft = screenSize.width - rightMargin - fabTargetWidth;
+    final double targetTop = screenSize.height - bottomMargin - fabTargetHeight;
+
+    final Rect targetRect = Rect.fromLTWH(
+      targetLeft,
+      targetTop,
+      fabTargetWidth,
+      fabTargetHeight,
+    );
+
+    return RepaintBoundary(
+      child: GestureDetector(
+        onTap: _handleClose,
+        child: Stack(
+          children: [
+            // Dark Backdrop Layer with decoupled timing tracks
+            AnimatedBuilder(
+              animation: Listenable.merge(
+                  [widget.routeAnimation, _localCloseController]),
+              builder: (context, child) {
+                final double bgOpacity = _isClosing
+                    ? (1.0 - _exitAnimation.value)
+                    : widget.routeAnimation.value;
+                return Container(
+                  color: Color.lerp(
+                      Colors.transparent, const Color(0x66000000), bgOpacity),
+                );
+              },
+            ),
+
+            // The Primary Moving FAB Button Node
+            AnimatedBuilder(
+              animation: Listenable.merge(
+                  [widget.routeAnimation, _localCloseController]),
+              builder: (context, child) {
+                final double spatialT = widget.expressiveFastSpatial
+                    .transform(widget.routeAnimation.value);
+                final double rotationT =
+                    Curves.easeOutCubic.transform(widget.routeAnimation.value);
+
+                final Rect currentRect = _isClosing
+                    ? Rect.lerp(
+                        targetRect, widget.sourceRect, _exitAnimation.value)!
+                    : Rect.lerp(widget.sourceRect, targetRect, spatialT)!;
+
+                final double borderRadiusValue = _isClosing
+                    ? BorderRadius.lerp(BorderRadius.circular(30),
+                            BorderRadius.circular(24), _exitAnimation.value)!
+                        .topLeft
+                        .x
+                    : BorderRadius.lerp(BorderRadius.circular(24),
+                            BorderRadius.circular(30), spatialT)!
+                        .topLeft
+                        .x;
+
+                final double currentRotation = _isClosing
+                    ? (0.125 * (1.0 - _exitAnimation.value))
+                    : (0.125 * rotationT);
+
+                return Positioned(
+                  left: currentRect.left,
+                  top: currentRect.top, // Fixed layout bounds across platforms
+                  width: currentRect.width,
+                  height: currentRect.height,
+                  child: SizedBox(
+                    width: currentRect.width,
+                    height: currentRect.height,
+                    child: FloatingActionButton(
+                      backgroundColor: theme.colorScheme.primary,
+                      foregroundColor: theme.colorScheme.onPrimary,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(borderRadiusValue),
+                      ),
+                      onPressed: _handleClose,
+                      child: RotationTransition(
+                        turns: AlwaysStoppedAnimation(currentRotation),
+                        child: const Icon(
+                          Icons.add,
+                          size: 28,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+
+            // Contextual Stacked Option List Menu
+            Positioned(
+              right: rightMargin,
+              bottom: bottomMargin + fabTargetHeight + 16,
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: screenSize.width - rightMargin * 2,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    _buildStaggeredButton(
+                      context: context,
+                      icon: Symbols.send,
+                      label: 'Chat',
+                      index: 2,
+                      exitAnimation: _exitAnimation,
+                      theme: theme,
+                    ),
+                    const SizedBox(height: 4),
+                    _buildStaggeredButton(
+                      context: context,
+                      icon: Symbols.restart_alt,
+                      label: 'Slice',
+                      index: 0,
+                      exitAnimation: _exitAnimation,
+                      theme: theme,
+                    ),
+                    const SizedBox(height: 4),
+                    _buildStaggeredButton(
+                      context: context,
+                      icon: Icons.add,
+                      label: 'Post',
+                      index: 1,
+                      exitAnimation: _exitAnimation,
+                      theme: theme,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStaggeredButton({
+    required BuildContext context,
+    required IconData icon,
+    required String label,
+    required int index,
+    required Animation<double> exitAnimation,
+    required ThemeData theme,
+  }) {
+    return AnimatedBuilder(
+      animation:
+          Listenable.merge([widget.routeAnimation, _localCloseController]),
+      builder: (context, child) {
+        final double spatialValue;
+        final double opacityValue;
+
+        if (_isClosing) {
+          spatialValue = 1.0 - exitAnimation.value;
+          opacityValue = 1.0 - exitAnimation.value;
+        } else {
+          final double startInterval = index * 0.1;
+          final double endInterval = (0.76 + index * 0.1).clamp(0.0, 1.0);
+
+          final double rawProgress = widget.routeAnimation.value;
+          final double buttonProgress =
+              ((rawProgress - startInterval) / (endInterval - startInterval))
+                  .clamp(0.0, 1.0);
+          spatialValue = widget.expressiveFastSpatial.transform(buttonProgress);
+
+          final double opacityProgress =
+              ((rawProgress - startInterval) / 0.12).clamp(0.0, 1.0);
+          opacityValue =
+              widget.expressiveFastEffects.transform(opacityProgress);
+        }
+
+        const double maxButtonWidth = 140.0;
+        const double buttonHeight = 56.0;
+        final double currentWidth = maxButtonWidth * spatialValue;
+
+        return Opacity(
+          opacity: opacityValue.clamp(0.0, 1.0),
+          child: Container(
+            height: buttonHeight,
+            width: currentWidth,
+            decoration: BoxDecoration(
+              color: theme.colorScheme.primaryFixedDim,
+              borderRadius: BorderRadius.circular(100),
+            ),
+            clipBehavior: Clip.antiAlias,
+            child: OverflowBox(
+              minWidth: maxButtonWidth,
+              maxWidth: maxButtonWidth,
+              minHeight: buttonHeight,
+              maxHeight: buttonHeight,
+              alignment: Alignment.centerRight,
+              child: FilledButton.icon(
+                style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.transparent),
+                  elevation: WidgetStatePropertyAll(0),
+                ),
+                onPressed: () {},
+                label: Text(
+                  label,
+                  style: GoogleFonts.manrope(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w800,
+                    color: theme.colorScheme.onPrimaryFixedVariant,
+                  ),
+                ),
+                icon: Icon(
+                  icon,
+                  size: 24,
+                  weight: 700,
+                  color: theme.colorScheme.onPrimaryFixedVariant,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
